d26ee7a93ec0a686a528d12256c289e3d9e0cf4c
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2020-08-20 13:17:37 +0200

    added a couple of actual unit tests

--- lfpy_forward_models/tests/test_module.py
+++ lfpy_forward_models/tests/test_module.py
@@ -14,16 +14,50 @@ GNU General Public License for more details.
 
 """
 
-
-import os
 import unittest
 import numpy as np
-import lfpy_forward_models
+import lfpy_forward_models as lfp
+
+
+def get_cell(n_seg=4):
+    cell = lfp.CellGeometry(x=np.array([[0.]*2]*n_seg),
+                            y=np.array([[0.]*2]*n_seg),
+                            z=np.array([[1.*x, 1.*(x+1)]
+                                        for x in range(n_seg)]),
+                            diam=np.array([1.]*n_seg))
+    return cell
+
 
 class TestSuite(unittest.TestCase):
     """
     test methods and modules
     """
-    def test_testsuite_00(self):
+    def test_TestSuite_00(self):
         '''test TestSuite'''
         self.assertTrue(True)
+
+    def test_LinearModel_00(self):
+        '''test LinearModel'''
+        cell = get_cell(n_seg=4)
+        lm = lfp.LinearModel(cell)
+        M = lm.get_response_matrix()
+
+        imem = np.arange(24).reshape((4, -1))
+
+        self.assertTrue(np.all(imem == np.dot(M, imem)))
+
+    def test_CurrentDipoleMoment_00(self):
+        '''test CurrentDipoleMoment'''
+        cell = get_cell(n_seg=3)
+        cdm = lfp.CurrentDipoleMoment(cell)
+        M = cdm.get_response_matrix()
+
+        imem = np.array([[-1., 1.],
+                         [0., 0.],
+                         [1., -1.]])
+
+        P = np.dot(M, imem)
+
+        P_gt = np.array([[0., 0.], [0., 0.], [2., -2.]])
+
+        self.assertTrue(np.all(P_gt == P))
