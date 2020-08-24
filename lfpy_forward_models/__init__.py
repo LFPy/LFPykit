fb5b7735dbbf14adf92ae3738ce7169c60b22302
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2020-08-24 15:07:59 +0200

    docstring update

--- lfpy_forward_models/__init__.py
+++ lfpy_forward_models/__init__.py
@@ -12,17 +12,39 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 :Classes:
-  * CellGeometry - Base class representing a multicompartment neuron geometry for subclassing
-  * LinearModel - Base class representing a generic forward model for subclassing
-  * RecExtElectrode - Class for performing simulations of extracellular potentials
-  * RecMEAElectrode - Class for performing simulations of in vitro (slice) extracellular potentials
-  * OneSphereVolumeConductor - For computing extracellular potentials within and outside a homogeneous sphere
-  * FourSphereVolumeConductor - For computing extracellular potentials in 4-sphere model (brain, CSF, skull, scalp)
-  * InfiniteVolumeConductor - To compute extracellular potentials with current dipoles in infinite volume conductor
-  * MEG - Class for computing magnetic field from current dipole moment
+  * CellGeometry:
+        Base class representing a multicompartment neuron geometry
+        for subclassing
+  * LinearModel:
+        Base class representing a generic forward model
+        for subclassing
+  * CurrentDipoleMoment:
+        Class for predicting current dipole moments
+  * PointSourcePotential:
+        Class for predicting extracellular potentials
+        assuming point sources and contacts
+
+  :Classes to be implemented:
+  * RecExtElectrode:
+        Class for simulations of extracellular potentials
+  * RecMEAElectrode:
+        Class for simulations of in vitro (slice) extracellular
+        potentials
+  * OneSphereVolumeConductor:
+        For computing extracellular potentials within
+        and outside a homogeneous sphere
+  * FourSphereVolumeConductor:
+        For computing extracellular potentials in
+        4-sphere model (brain, CSF, skull, scalp)
+  * InfiniteVolumeConductor:
+        To compute extracellular potentials with current
+        dipoles in infinite volume conductor
+  * MEG:
+        Class for computing magnetic field from current dipole moments
 :Modules:
   * cell
   * models
+  * lfpcalc
 """
 
 from .version import version as __version__
