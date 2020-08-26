4734251cb499c687c582ab317a4abeece52e1c5a
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2020-08-26 11:52:56 +0200

    added one more test from LFPy

--- lfpy_forward_models/tests/test_module.py
+++ lfpy_forward_models/tests/test_module.py
@@ -422,18 +422,18 @@ class TestSuite(unittest.TestCase):
                                              [0., 0.]]),
                                  z=np.array([[0, 0],
                                              [0, 0],
-                                             [0, 2.81668764e-15],
-                                             [0, 4.22503146e-15],
-                                             [0, 5.63337528e-15],
-                                             [0, 7.04171910e-15],
-                                             [0, 1.00000000e+01],
-                                             [1.00000000e+01, 2.00000000e+01],
-                                             [2.00000000e+01, 3.00000000e+01],
-                                             [3.00000000e+01, 4.00000000e+01],
-                                             [4.00000000e+01, 5.00000000e+01],
-                                             [0, -1.00000000e+01],
-                                             [-1.00000000e+01, -2.00000000e+01],
-                                             [-2.00000000e+01, -3.00000000e+01]]),
+                                             [0, 0],
+                                             [0, 0],
+                                             [0, 0],
+                                             [0, 0],
+                                             [0, 0],
+                                             [10, 20],
+                                             [20, 30],
+                                             [30, 40],
+                                             [40, 50],
+                                             [0, -10],
+                                             [-10, -20],
+                                             [-20, -30]]),
                                  d=np.zeros(14))
 
         stick.z = stick.z + 1
@@ -463,3 +463,89 @@ class TestSuite(unittest.TestCase):
         stick.z = np.zeros_like(stick.z) + 202
         MEA = lfp.RecMEAElectrode(stick, **electrodeParams)
         np.testing.assert_raises(RuntimeError, MEA._test_cell_extent)
+
+    def test_RecMEAElectrode_01(self):
+        '''test return_comp_outside_slice'''
+        electrodeParams = {
+            'sigma_T': 0.3,
+            'sigma_S': 1.5,
+            'sigma_G': 0.0,
+            'h': 200,
+            'x': np.linspace(0, 1000, 11),
+            'y': np.zeros(11),
+            'z': np.zeros(11),
+            'method': "pointsource",
+            'squeeze_cell_factor': None,
+        }
+
+        stick = lfp.CellGeometry(x=np.array([[-17.5, 17.5],
+                                             [0., 23.],
+                                             [23., 46.],
+                                             [46., 69.],
+                                             [69., 92.],
+                                             [92., 115.],
+                                             [115., 125.],
+                                             [125., 135.],
+                                             [135., 145.],
+                                             [145., 155.],
+                                             [155., 165.],
+                                             [115., 118.33333333],
+                                             [118.33333333, 121.66666667],
+                                             [121.66666667, 125.]]),
+                                 y=np.array([[0., 0.],
+                                             [0., 0.],
+                                             [0., 0.],
+                                             [0., 0.],
+                                             [0., 0.],
+                                             [0., 0.],
+                                             [0., 4.],
+                                             [4., 8.],
+                                             [8., 12.],
+                                             [12., 16.],
+                                             [16., 20.],
+                                             [0., 0.],
+                                             [0., 0.],
+                                             [0., 0.]]),
+                                 z=np.array([[0, 0],
+                                             [0, 0],
+                                             [0, 0],
+                                             [0, 0],
+                                             [0, 0],
+                                             [0, 0],
+                                             [0, 0],
+                                             [10, 20],
+                                             [20, 30],
+                                             [30, 40],
+                                             [40, 50],
+                                             [0, -10],
+                                             [-10, -20],
+                                             [-20, -30]]),
+                                 d=np.zeros(14))
+        stick.z = stick.z + 100
+        MEA = lfp.RecMEAElectrode(stick, **electrodeParams)
+        np.testing.assert_raises(RuntimeError, MEA._return_comp_outside_slice)
+        true_bad_comp = np.array([2, 3, 6])
+
+        stick.z[true_bad_comp, 0] = 1000
+        bad_comp, reason = MEA._return_comp_outside_slice()
+        np.testing.assert_equal(reason, "zstart above")
+        np.testing.assert_equal(true_bad_comp, bad_comp)
+        stick.z[true_bad_comp, 0] = 100
+
+        stick.z[true_bad_comp, 0] = -1000
+        bad_comp, reason = MEA._return_comp_outside_slice()
+        np.testing.assert_equal(reason, "zstart below")
+        np.testing.assert_equal(true_bad_comp, bad_comp)
+        stick.z[true_bad_comp, 0] = 100
+
+        stick.z[true_bad_comp, -1] = 1000
+        bad_comp, reason = MEA._return_comp_outside_slice()
+        np.testing.assert_equal(reason, "zend above")
+        np.testing.assert_equal(true_bad_comp, bad_comp)
+        stick.z[true_bad_comp, -1] = 100
+
+        stick.z[true_bad_comp, -1] = -1000
+        bad_comp, reason = MEA._return_comp_outside_slice()
+        np.testing.assert_equal(reason, "zend below")
+        np.testing.assert_equal(true_bad_comp, bad_comp)
+        stick.z[true_bad_comp, -1] = 100
