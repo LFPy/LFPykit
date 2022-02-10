8ce996441629d3d129c8f16ade1629231c535ca5
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-02-10 09:41:38 +0100

    Add py=3.10 to build matrix. No neuron install

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Operating System :: OS Independent',
         'Topic :: Scientific/Engineering',
