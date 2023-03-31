e5156c07d447227625341509a4974bd4d6d68f67
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2023-03-31 17:57:36 +0200

    Revert "Added automatic download of New York Head model, if query can not be answered because of no input device (#190)" (#191)
    This reverts commit 638071b008a1ef1fd86b03f779da7bc27edfdc04.

--- lfpykit/eegmegcalc.py
+++ lfpykit/eegmegcalc.py
@@ -1409,11 +1409,7 @@ class NYHeadModel(Model):
             from urllib.request import urlopen
             import ssl
             print("New York head-model file not found: %s" % self.head_file)
-            try:
-                yn = input(f"Download as {self.head_file} (710 MB)? [y/n]: ")
-            except StdinNotImplementedError:
-                # If no input device is available, we just go ahead and download.
-                yn = 'y'
+            yn = input(f"Download as {self.head_file} (710 MB)? [y/n]: ")
             if yn == 'y':
                 print("Now downloading. This might take a while ...")
                 nyhead_url = 'https://www.parralab.org/nyhead/sa_nyhead.mat'
